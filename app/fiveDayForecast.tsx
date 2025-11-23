58ed41677ba1cd955c6762629948f4afc7f165d5
Xander Mulligan <xander.i.mulligan@gmail.com>
2025-11-23 15:31:32 -0700

    start

---
+++ app/fiveDayForecast.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const fiveDayForecast = () => {
+  return (
+    <View>
+      <Text>fiveDayForecast</Text>
+    </View>
+  )
+}
+
+export default fiveDayForecast
+
+const styles = StyleSheet.create({})
