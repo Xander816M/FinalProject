bb00c37928f3ba900d8376c2c9ed0ae456a59ed9
Xander Mulligan <xander.i.mulligan@gmail.com>
2025-12-08 14:15:58 -0700

    forecast improvements

--- api/getWeather.ts
+++ api/getWeather.ts
@@ -1,6 +1,10 @@
+const BASE_URL = 
+  process.env.WEATHER_API_URL || "http://api.weatherapi.com/v1/forecast.json?key=cbd03f903c5949e993505852250612&q=";
+
+
 export const fetchForecast = async () => {
   try {
-    const response = await fetch("http://api.weatherapi.com/v1/forecast.json?key=cbd03f903c5949e993505852250612&q=Calgary&days=7");
+    const response = await fetch(`${BASE_URL}Calgary&days=7`);
     const data = await response.json();
     return data;
   } catch (error) {
@@ -11,7 +15,7 @@ export const fetchForecast = async () => {
 
 export const fetchCurrentWeather = async () => {
   try {
-    const response = await fetch("https://api.weatherapi.com/v1/current.json?key=cbd03f903c5949e993505852250612&q=London");
+    const response = await fetch(`${BASE_URL}London`);
     const data = await response.json();
     return data;
   } catch (error) {
